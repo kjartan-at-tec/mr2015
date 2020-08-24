bc203673884d148352507144ac24cf1be718dc6f
Kjartan Halvorsen <kjartan.halvorsen@gmail.com>
2020-08-24 15:50:32 -0500

    Material for second-order systems

---
+++ second-order-models/slides/auto/lecture-modeling-second-order.el
@@ -0,0 +1,46 @@
+(TeX-add-style-hook
+ "lecture-modeling-second-order"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "presentation" "aspectratio=169")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "khpreamble")
+   (TeX-add-symbols
+    "shift"
+    "Tcnst"
+    "tdelay"
+    "ggain"
+    "uampl")
+   (LaTeX-add-labels
+    "sec:org6b44978"
+    "sec:org6304eb7"
+    "sec:orga95b64a"))
+ :latex)
+
