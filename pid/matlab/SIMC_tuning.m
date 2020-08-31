9298c0b8ce3afe36bfc113968bd165837203bb9d
Kjartan Halvorsen <kjartan.halvorsen@gmail.com>
2020-08-31 15:47:37 -0500

    PID material

---
+++ pid/matlab/SIMC_tuning.m
@@ -0,0 +1,28 @@
+%% PID tuning of the double tank using SIMC
+% Kjartan Halvorsen
+% 2020-08-27
+
+
+h0 = 1.275;
+uf = 0.015;
+yf = 0.88;
+K = yf/uf;
+y1 = 0.2645*yf;
+tau = 58.1; % Observed from bump test
+
+% Tuning parameters
+%tauc = tau*0.5; % Aggressive
+%tauc = tau; % Nominal
+tauc = 2*tau; % Cautious
+
+% Series form
+Kc = tau/(tauc*K);
+tauI = tau;
+tauD = tau;
+
+% Parallel form
+kc = Kc * (tauI+tauD)/tauI;
+taui = tauI + tauD;
+taud = tauI*tauD/(tauI+tauD);
+
+N=20;
--- pid/slides/auto/lecture-pid.el
+++ pid/slides/auto/lecture-pid.el
@@ -6,13 +6,13 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -40,9 +40,10 @@
     "uampl"
     "TT")
    (LaTeX-add-labels
-    "sec:org3dadddc"
-    "sec:org1f7c4a6"
-    "sec:org9b33ae9")
+    "sec:org1ec931e"
+    "sec:org4283999"
+    "sec:orgb463b90"
+    "sec:orgf9d0d46")
    (LaTeX-add-xcolor-definecolors
     "ppc"
     "iic"
