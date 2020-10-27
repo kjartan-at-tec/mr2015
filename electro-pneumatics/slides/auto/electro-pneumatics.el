0b89410cebc62732a65ce894aaf554f129bf5cc7
Kjartan Halvorsen <kjartan.halvorsen@gmail.com>
2020-10-26 19:06:00 -0600

    New stuf

--- electro-pneumatics/slides/auto/electro-pneumatics.el
+++ electro-pneumatics/slides/auto/electro-pneumatics.el
@@ -6,13 +6,13 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -39,11 +39,11 @@
     '("etimer" 2)
     '("coil" 1))
    (LaTeX-add-labels
-    "sec:org7b8bb50"
-    "sec:orgf1a871a"
-    "sec:orgdc393fa"
-    "sec:orgea5e4dc"
-    "sec:orgf4de0e1"
-    "sec:orgfc32047"))
+    "sec:orgcdcd0d1"
+    "sec:orgfcfb1f8"
+    "sec:orgf584dcf"
+    "sec:orgc58c296"
+    "sec:org9bb80ed"
+    "sec:orge19ad7e"))
  :latex)
 
